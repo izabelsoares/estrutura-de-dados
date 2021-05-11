50d2c338be486f5285287ca1ad7077e85f020d33
izabelsoares <izabelsoaresbds@gmail.com>
2021-05-11 10:23:18 -0300

    primeiro exercicio de logica

---
+++ index.js
@@ -0,0 +1,12 @@
+const name ="Izabel";
+let age = 18;
+let anoAtual = 2021;
+
+
+console.log("meu ano de nascimento é" ,anoAtual - age)
+
+let souHumano = true;
+const number1 = 5681655;
+const number2 = 2626658;
+
+console.log(number1 + number2)
